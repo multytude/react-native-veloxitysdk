de3ceac75b1b859ae6eca97395f55da2de9f09be
emre-multytude <emre@multytude.com>
2022-11-25 14:54:08 +0300

    fix: removed override for build error

--- android/src/main/java/net/veloxity/nativesdk/VeloxityPackage.java
+++ android/src/main/java/net/veloxity/nativesdk/VeloxityPackage.java
@@ -27,7 +27,7 @@ public class VeloxityPackage implements ReactPackage {
       return modules;
    }
 
-   @Override
+   // @Override
    public List<Class<? extends JavaScriptModule>> createJSModules() {
       return null;
    }
